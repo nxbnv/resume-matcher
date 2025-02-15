763638c86f43ffa804a9d30337eacdae6fd0f9fd
coder-super <rohanhota0@gmail.com>
2025-02-15 18:22:20 +0530

    Saved local changes before pulling

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -89,7 +89,7 @@ try:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
-            creds = flow.run_local_server(port=5500)
+            creds = flow.run_local_server(port=5000)
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
