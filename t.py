5c5ec2db956c3f09b65d5c55819b70dc788d3023
coder-super <rohanhota0@gmail.com>
2025-02-16 19:22:23 +0530

    done

--- t.py
+++ t.py
@@ -1,10 +1,4 @@
-import pickle
+from transformers import AutoModel
 
-# Try loading locally to see if the file itself is corrupted
-with open("models/vectorizer.pkl", "rb") as f:
-    obj = pickle.load(f)
-    print("Vectorizer loaded successfully!")
-
-with open("models/bert_model.pkl", "rb") as f:
-    obj = pickle.load(f)
-    print("BERT Model loaded successfully!")
+model = AutoModel.from_pretrained("rohan57/mymodel")
+print(model)
